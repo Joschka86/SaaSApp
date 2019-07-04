03c6143180e88539cbabc7c8aa24d255e6c8602d
Joschka86 <fuerup@gmail.com>
2019-07-04 06:47:58 +0000

    Added contact create action to save to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
-end
+  
+  def create 
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "Nachricht gesendet."
+    else 
+      redirect_to new_contact_path. notice:"Ein Fehler ist aufgetreten."
+    end
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:Name, :Email, :Kommentar)
+    end
+  end
